dbc58e29fa54b29c4397d73ebb43b90c86de0fcb
Andrea Boriero <andrea@hibernate.org>
2020-05-15 10:17:18 +0100

    Test for #8932 Hibernate Lazy load issue

---
+++ integration-tests/hibernate-orm-panache/src/main/java/io/quarkus/it/panache/Content.java
@@ -0,0 +1,17 @@
+package io.quarkus.it.panache;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity(name = "content")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Content extends EntityBase {
+    @Column(nullable = false)
+    public String name;
+
+    //	public String getName() {
+    //		return name;
+    //	}
+}
